a08a5ef32b2c60e6a40c314a0a1568eb2a0ced78
Jenil Bhavsar <jenil.bhavsar@gmail.com>
2022-11-20 12:06:29 -0500

    fix(task-two): updated types

--- task-two/src/app/card/card.model.ts
+++ task-two/src/app/card/card.model.ts
@@ -2,10 +2,6 @@ export interface Item {
     url: String;
     title: String;
     description: String;
-    isNoIndex?: Boolean;
-    category: Category;
-}
-
-export interface Category {
-    [key: string]: String;
+    isNoIndex?: Boolean | null;
+    category: any;
 }
